b3f8ac3580f77b10b60b589dec3d62a6b42f9b1b
Omotara Akinsowon <88031800+Omotarara@users.noreply.github.com>
2021-11-12 01:55:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const calculator = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
-};
+// program for a simple calculator
 
-function updateDisplay() {
-  const display = document.querySelector('.calculator-screen');
-  display.value = calculator.displayValue;
+// take the operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// take the operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+// using if...else if... else
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
 }
 
-updateDisplay();
+// display the result
+console.log(`${number1} ${operator} ${number2} = ${result}`);
